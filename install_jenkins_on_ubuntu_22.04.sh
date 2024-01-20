5ad051ad69725729fe9468896dac3ff7343bdc68
Krishnadhas N K <108367225+githubofkrishnadhas@users.noreply.github.com>
2024-01-20 21:57:29 +0530

    Update install_jenkins_on_ubuntu_22.04.sh

--- install_jenkins_on_ubuntu_22.04.sh
+++ install_jenkins_on_ubuntu_22.04.sh
@@ -13,7 +13,7 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
 # Update package information again to include the Jenkins repository
-sudo apt update-y
+sudo apt update -y
 
 # Install Jenkins
 sudo apt install -y jenkins
